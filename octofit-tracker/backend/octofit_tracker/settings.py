5f3426eac6535b196bc2d1b33dea6b7ada4a1d5a
Arpit Saxena <40694080+Arpit000@users.noreply.github.com>
2025-04-08 13:04:26 +0000

    Update views.py and settings.py for codespace URL and allow host access

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*2_phnw=muz$#y@(vd3g@dzl6mitmcs-vcw8k&3kszotsloz61
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'psychic-space-fortnight-vg94w9x4pw62xqgx-8000.app.github.dev']
 
 
 # Application definition
